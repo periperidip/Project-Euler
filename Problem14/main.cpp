53da332acdedcfd40314f197269101b8ae69174b
Shourya Shukla <43680618+g0pn1k@users.noreply.github.com>
2019-05-18 13:45:29 +0530

    Update

--- Problem14/main.cpp
+++ Problem14/main.cpp
@@ -1,21 +1,69 @@
-#include <iostream>
+#include<iostream>
 
-int main() {
-  long int n=14,c=0;
-  //long int A[1000000];
- for(n=13;n<16;n++)
-  {
-    while(n!=1)
-    {
-      if(n%2==0)
-      {n=n/2;}
-      else
-      {n=((3*n)+1);}
-      
-      c++;
+int main()
+{
+const int number = 1000000;
+ 
+long sequenceLength = 0;
+long startingNumber = 0;
+long sequence;
+ 
+for (int i = 2; i <= number; i++) {
+    int length = 1;
+    sequence = i;
+    while (sequence != 1) {
+        if ((sequence % 2) == 0) {
+            sequence = sequence / 2;
+        } else {
+            sequence = sequence * 3 + 1;
+        }
+        length++;
     }
-  }
-  std::cout<<c+1;
-   
-
+ 
+    //Check if sequence is the best solution
+    if (length > sequenceLength) {
+        sequenceLength = length;
+        startingNumber = i;
+    }
 }
+std::cout<<startingNumber;
+}
+
+
+/*  CACHING METHOD. HIGHLY EFFICIENT  */ 
+
+
+
+// const int number = 1000000;
+ 
+// int sequenceLength = 0;
+// int startingNumber = 0;
+// long sequence;
+ 
+// int[] cache = new int[number + 1];
+// //Initialise cache
+// for (int i = 0; i < cache.Length; i++) {
+//     cache[i] = -1;
+// }
+// cache[1] = 1;
+ 
+// for (int i = 2; i <= number; i++) {
+//     sequence = i;
+//     int k = 0;
+//     while (sequence != 1 && sequence >= i) {
+//         k++;
+//         if ((sequence % 2) == 0) {
+//             sequence = sequence / 2;
+//         } else {
+//             sequence = sequence * 3 + 1;
+//         }
+//     }
+//     //Store result in cache
+//     cache[i] = k + cache[sequence];
+ 
+//     //Check if sequence is the best solution
+//     if (cache[i] > sequenceLength) {
+//         sequenceLength = cache[i];
+//         startingNumber = i;
+//     }
+// }
